905fe803b997d6978f203c33cdd79fc38c53115e
X1011 <XX1011@gmail.com>
2023-04-29 00:47:28 -0400

    inline simple functions

--- descript.py
+++ descript.py
@@ -7,17 +7,11 @@ files = sys.argv[1:]
 def get_basename(file):
 	return path.splitext(file)[0]
 
-def is_regular_file(file):
-	return path.isfile(file)
-
 def has_same_basename(file, other_file):
 	return get_basename(file) == get_basename(other_file)
 
-def is_not_same_file(file, other_file):
-	return file != other_file
-
 def is_duplicate(file, other_file):
-	return is_regular_file(other_file) and has_same_basename(file, other_file) and is_not_same_file(file, other_file)
+	return path.isfile(other_file) and has_same_basename(file, other_file) and file != other_file
 
 def get_duplicates(file):
 	return [other_file for other_file in listdir('.') if is_duplicate(file, other_file)]
